008e026cbe88e640d3c46327fc63596f2513e315
fabriciofeitosacastro@gmail.com <fabriciofeitosacastro@gmail.com>
2024-04-01 22:48:56 -0300

    Ajustado validação do button publicar

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -2,12 +2,23 @@
 import style from './Comment.module.css'
 import { Trash, ThumbsUp } from "@phosphor-icons/react";
 import {Avatar} from '../avatar/Avatar'
+import { useState } from 'react';
 
 const Comment = ({content, deleteComment}) => {
 
+  /* -------------------- States ------------------- */
+
+    const [likeCount, setLikeCount] = useState(() => 0)
+
+  /* ----------- Functions ------------------------- */
+  
   function handleDeleteComment() {
    
      deleteComment(content)
+  }
+
+  function handleLikeCount() {
+    setLikeCount(() => likeCount + 1)
   }
 
  
@@ -37,9 +48,9 @@ const Comment = ({content, deleteComment}) => {
           </p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeCount}>
             <ThumbsUp size={20} />  
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         
         </footer>
